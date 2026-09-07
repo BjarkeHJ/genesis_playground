9b807359375e0d13c2d003f04904752b7d8c033b
BjarkeHJ <bjarke_holm@hotmail.com>
2026-09-07 14:16:30 +0200

    ..

--- build_deps.sh
+++ build_deps.sh
@@ -18,8 +18,10 @@ fi
 
 source .venv/bin/activate
 
+# Install dependencies into virtual environment
 pip install pyyaml || { echo "Error: failed to install pyyaml" >&2; $RET 1; }
 pip install pyvista || { echo "Error: failed to install pyvista" >&2; $RET 1; }
+pip install torch torchvision || { echo "Error: failed to install torch" >&2; $RET 1; }
 cd external/genesis-world && pip install -e . || { echo "Error: failed to install genesis-world" >&2; $RET 1; }
 
 cd -
