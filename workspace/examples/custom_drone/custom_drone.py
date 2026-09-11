b100d7efd539dbf06390ee36f9eb090162a8075b
BjarkeHJ <bjarke_holm@hotmail.com>
2026-09-11 14:38:54 +0200

    sm update

--- workspace/examples/custom_drone/custom_drone.py
+++ workspace/examples/custom_drone/custom_drone.py
@@ -4,6 +4,8 @@ import genesis as gs
 import numpy as np
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+REPO_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, "..", "..", ".."))
+MODEL_PATH = os.path.join(REPO_ROOT, "external", "models", "Starling2MaxURDF", "model", "starling2max.urdf")
 
 # Init scene - MUST be the first thing done
 gs.init(backend=gs.cpu)
@@ -25,9 +27,8 @@ scene.add_entity(gs.morphs.Plane())
 # Spawn in a drone entity (crazy flie)
 drone = scene.add_entity(
     gs.morphs.Drone(
-        file=os.path.join(SCRIPT_DIR, "drone/quadcopter.urdf"),
+        file=os.path.join(MODEL_PATH),
         pos=(0.0, 0.0, 2.0),  # meters, Z-up
-        prioritize_urdf_material=True,
         euler=(0.0, 0.0, 0.0),  # scipy extrinsic x-y-z, degrees
         propellers_link_name=("prop0_link", "prop1_link", "prop2_link", "prop3_link"),
         propellers_spin=(-1, 1, -1, 1),  # per propeller: -1 = CW, +1 = CCW
